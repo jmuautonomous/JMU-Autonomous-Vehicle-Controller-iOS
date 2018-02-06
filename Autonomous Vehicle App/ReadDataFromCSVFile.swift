f385d69117e39115620306b1f879c87c713782a6
Ben Gilliam <gilliabb@dukes.jmu.edu>
2018-02-06 17:07:23 -0500

    Added functions to pull csv file data into app

---
+++ Autonomous Vehicle App/ReadDataFromCSVFile.swift
@@ -0,0 +1,45 @@
+//
+//  readCSVFile.swift
+//  Autonomous Vehicle App
+//
+//  Created by tns_lab on 2/6/18.
+//
+
+import Foundation
+
+class ReadDataFromCSVFile {
+    
+    func readDataFromCSV(fileName:String, fileType: String)-> String!{
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: fileType)
+            else {
+                return nil
+        }
+        do {
+            var contents = try String(contentsOfFile: filepath, encoding: .utf8)
+            contents = cleanRows(file: contents)
+            return contents
+        } catch {
+            print("File Read Error for file \(filepath)")
+            return nil
+        }
+    }
+    
+    func cleanRows(file:String)->String{
+        var cleanFile = file
+        cleanFile = cleanFile.replacingOccurrences(of: "\r", with: "\n")
+        cleanFile = cleanFile.replacingOccurrences(of: "\n\n", with: "\n")
+        //        cleanFile = cleanFile.replacingOccurrences(of: ";;", with: "")
+        //        cleanFile = cleanFile.replacingOccurrences(of: ";\n", with: "")
+        return cleanFile
+    }
+    
+    func csv(data: String) -> [[String]] {
+        var result: [[String]] = []
+        let rows = data.components(separatedBy: "\n")
+        for row in rows {
+            let columns = row.components(separatedBy: ";")
+            result.append(columns)
+        }
+        return result
+    }
+}
--- Autonomous Vehicle App/ViewController.swift
+++ Autonomous Vehicle App/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var menuButton2: UIBarButtonItem?
     @IBOutlet weak var menuButton3: UIBarButtonItem?
     @IBOutlet var mapView: MKMapView?
+    @IBOutlet weak var ScrollView: UIScrollView!
     
     
     let locationManager = CLLocationManager()
@@ -24,6 +25,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         mapView?.delegate = self
         mapView?.showsUserLocation = true
+        
+        //set scrollview height
+        ScrollView?.contentSize.height = 1000
         
         //requesting user location
         locationManager.requestAlwaysAuthorization()
@@ -62,6 +66,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     //Go to button onClick functions
     @IBAction func goToButton1(_ sender: UIButton) {
         goAlert(buttonNo: 1)
+        printCSVValues()
     }
     @IBAction func goToButton2(_ sender: UIButton) {
         goAlert(buttonNo: 2)
@@ -85,6 +90,48 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let namePredicate = NSPredicate(format: "ID = %@", ID)
         return [array.filter {namePredicate.evaluate(with: $0)}[0]]
     }*/
+    
+    //CSV stuff
+    func readDataFromCSV(fileName:String, fileType: String)-> String!{
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: fileType)
+            else {
+                return nil
+        }
+        do {
+            var contents = try String(contentsOfFile: filepath, encoding: .utf8)
+            contents = cleanRows(file: contents)
+            return contents
+        } catch {
+            print("File Read Error for file \(filepath)")
+            return nil
+        }
+    }
+    
+    func cleanRows(file:String)->String{
+        var cleanFile = file
+        cleanFile = cleanFile.replacingOccurrences(of: "\r", with: "\n")
+        cleanFile = cleanFile.replacingOccurrences(of: "\n\n", with: "\n")
+        //        cleanFile = cleanFile.replacingOccurrences(of: ";;", with: "")
+        //        cleanFile = cleanFile.replacingOccurrences(of: ";\n", with: "")
+        return cleanFile
+    }
+    
+    func csv(data: String) -> [[String]] {
+        var result: [[String]] = []
+        let rows = data.components(separatedBy: "\n")
+        for row in rows {
+            let columns = row.components(separatedBy: ";")
+            result.append(columns)
+        }
+        return result
+    }
+    
+    func printCSVValues() {
+        var data = readDataFromCSV(fileName: "path_isat_to_lakeview", fileType: "csv")
+        data = cleanRows(file: data!)
+        let csvRows = csv(data: data!)
+        print(csvRows[1][0])
+    }
     
     // jmu*Location = various coordinates of JMU POIs
     let jmuXlabsLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.431928, -78.875965)
